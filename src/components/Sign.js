2717040fe8c04a24734280f228a835fe8ed6d797
A-Little-Hat <soumyasarkar309@gmail.com>
2022-01-27 12:14:00 +0530

    signInWithGooglePopUp logic done

--- src/components/Sign.js
+++ src/components/Sign.js
@@ -1,11 +1,27 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import GoogleButton from 'react-google-button';
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+
 import '../App.css';
 import Signlog from './Signlog';
+import { fireAuth } from '../firebase/main.firebase';
+
 // import {useCookies} from 'react-cookie';
 
 export default function Sign(props) {
     const [sign, setSign] = useState('login');
-
+    const signInWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(fireAuth, provider)
+        .then((user) => {
+            console.log(user);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+    
     return (
         <div className='vh-100 back'>
             <div className="container py-5 h-100">
@@ -17,13 +33,13 @@ export default function Sign(props) {
                                     <img
                                         src="https://images.pexels.com/photos/830829/pexels-photo-830829.jpeg?w=400&h=600&fit=crop"
                                         alt="login form"
-                                        className="img-fluid radii" 
+                                        className="img-fluid radii"
                                     />
                                 </div>
                                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                     <div className="card-body p-4 p-lg-5 text-black">
-                                       <Signlog sign={sign} setSign={setSign} setstatus={props.setstatus}/>
-                                        
+                                        <GoogleButton onClick={signInWithGoogle}/>
+                                        <Signlog sign={sign} setSign={setSign} setstatus={props.setstatus} />
                                     </div>
                                 </div>
                             </div>
--- src/firebase/main.firebase.js
+++ src/firebase/main.firebase.js
@@ -1,7 +1,10 @@
 import {initializeApp} from 'firebase/app';
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
 
 import firebaseConfig from "./firebase.config";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const storage = getStorage(firebaseApp);
+export const fireAuth = getAuth(firebaseApp);
