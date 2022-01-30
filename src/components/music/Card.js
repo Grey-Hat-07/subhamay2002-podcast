fcf6568ed94f6c5bc73b471b9e2f67defe3d1146
A-Little-Hat <soumyasarkar309@gmail.com>
2022-01-30 22:26:37 +0530

    card image resolved

--- src/components/music/Card.js
+++ src/components/music/Card.js
@@ -1,4 +1,4 @@
-import React,{ useEffect , useState } from 'react';
+import React from 'react';
 
 function Card({ data }) {
     const image = {
@@ -6,23 +6,11 @@ function Card({ data }) {
         height: 100,
         float: 'left'
     };
-    let imagedata;
-    const [imageurl, setimageurl] = useState('');
-    const fetchurl=async()=>{
-        let url = `https://api.unsplash.com/search/photos?page=1&query=${data.title}&client_id=4IH6RS0Y3Rami1N2k39QVhU13jg3N69YBfJActXaGvM`;
-        let response =await fetch(url);
-        imagedata = await response.json();
-        setimageurl(imagedata.results[0].urls.regular);
-        //console.log(imagedata);
-
-    }
-    useEffect(()=>{
-        fetchurl();
-    },[])
+    var imgLink = `https://source.unsplash.com/random/?${data.title}`;
     return (
         <div className="card">
             <div className="card-body">
-                <img src={imageurl} style={image} alt='aa' />
+                <img src={imgLink} style={image} alt="a" />
                 <h5 className="card-title">{data.title}</h5>
                 <p className="card-text">{data.publisher}</p>
                 <p className="card-text p"><marquee> {data.description} </marquee></p>
@@ -35,4 +23,3 @@ function Card({ data }) {
 }
 
 export default Card;
-
