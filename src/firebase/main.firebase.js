4df842a4b4f90b973d776f779e18f1bc25ab511b
A-Little-Hat <soumyasarkar309@gmail.com>
2022-01-31 21:35:39 +0530

    loading animation added

--- src/firebase/main.firebase.js
+++ src/firebase/main.firebase.js
@@ -4,7 +4,9 @@ import { getAuth } from "firebase/auth";
 
 
 import firebaseConfig from "./firebase.config";
+import { getFirestore } from '@firebase/firestore';
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const storage = getStorage(firebaseApp);
 export const fireAuth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
