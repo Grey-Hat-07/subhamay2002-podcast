46846c030e1f941e56b164b598750a5072c3037b
subhamay2002 <subhamaynath2002@gmail.com>
2022-02-18 20:35:34 +0530

    search added

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
-export default function Search() {
+export default function Search(props) {
     let ismobile = window.innerWidth < 768;
-    if(!ismobile) {
+    const getSearchTerm=()=>{
+        props.searchkeyword(props.inputEl.current.value);
+    }
 
     return <div className='border border-secondary rounded'>
         <div className="input-group">
-            <input type="text" className="form-control" placeholder="Search" />
+            <input type="text" className="form-control" placeholder="Search"
+            value={props.term} 
+            ref={props.inputEl}
+            onChange={getSearchTerm} />
             <button className="btn btn-info" type="button">search</button>
-        </div><div className='mt-2'>
-        <p className='text-muted'>Recommended List</p>
-        <p>Test1</p>
-        <p>Test2</p>
-        <p>Test3</p>
-        <p>Test4</p>
         </div>
-    </div>;}
-    else{
-        return<div> <div className='border border-secondary rounded'>
-        <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Search" />
-            <button className="btn btn-info" type="button">search</button>
-        </div></div>
-        </div>
-    }
+        {
+            ismobile ? null:<div className='mt-2'>
+            <p className='text-muted'>Recommended List</p>
+            <p>Test1</p>
+            <p>Test2</p>
+            <p>Test3</p>
+            <p>Test4</p>
+            </div>
+        }
+    </div>;
+    
+    
 }
--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import '../../App.css';
 import Search from '../Search';
 import Card from '../music/Card';
 
 
-export default function Dashboard({music}) {
+export default function Dashboard({ music }) {
+    const [search, setSearch] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const inputEl = useRef('');
+    const searchHandler = (searchterm) => {
+        setSearch(searchterm);
+        if (searchterm !== "") {
+            const newlist = music.filter((item) => {
+                return Object.values(item).join('').toLowerCase().includes(searchterm.toLowerCase());
+            });
+            setSearchResult(newlist);
+        }
+        else{
+            setSearchResult(music);
+        }
+    }
     return (
-    <div className='container'><br /><br />
-        <h1>Dashboard</h1>
-        <div className="row">
-            <div className="col-md-3">
-                <Search />
-            </div>
-            <div className="col-md-9">
-                {
-                    music.map((data,index) => {
-                        return < Card data={data} key={index} />
-                    })
-                }
+        <div className='container'><br /><br />
+            <h1>Dashboard</h1>
+            <div className="row">
+                <div className="col-md-3">
+                    <Search term={search} searchkeyword={searchHandler} inputEl={inputEl} />
+                </div>
+                <div className="col-md-9">
+                    {   search.length<1 ?
+                        music.map((data, index) => {
+                            return < Card data={data} key={index} />
+                        }):
+                        searchResult.length>0 ?
+                        searchResult.map((data, index) => {
+                            return < Card data={data} key={index} />
+                        }):
+                        <h1>No Result Found</h1>
+                    }
+                </div>
             </div>
         </div>
-    </div>
     )
 }
